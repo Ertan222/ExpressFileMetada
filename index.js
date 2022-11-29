d98502fd76e6773dda3d0d182baf66ca55c5b84e
Ertan Taner <ertantaner2000@gmail.com>
2022-11-29 15:51:58 +0300

    feat: File Metada Analyser Added

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var storage = multer.memoryStorage({});
+var upload = multer({storage: storage});
+
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post("/api/fileanalyse",upload.single("upfile"),(req,res) => {
+  let {originalname: fileName, mimetype: fileType, size: fileSize} = req.file;
+  
+  res.json({
+    name: `${fileName}`,
+    type: `${fileType}`,
+    size: `${fileSize}`
+  });
+
+});
 
 
 
